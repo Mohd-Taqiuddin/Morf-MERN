f709ed0b1d842d46e4781db26b50329bdb0a28a3
Mohd Taqiuddin <taqiuddin12345@gmail.com>
2022-04-14 03:11:11 +0530

    dashboard done

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -31,8 +31,20 @@ function Dashboard() {
                     <div className='addText'>Add Team Members</div>
                     <img src='	https://goglobiliti.com/assets/icons/Add-circle.svg' alt='add logo' />
                 </div>
-                <div className='team'>
-                    Members
+                <div className='teamMembers'>
+                    <div className='member sideBar__item2'>
+                        <div className='profileImg'>
+                            <img src='https://goglobiliti-sandbox.s3.amazonaws.com/innochamps/download%20(2).jpg' alt='profile' />
+                        </div>
+                        <div className='team'>
+                            <div className='memberName'>
+                                Member Name
+                            </div>
+                            <div className='memberTitle'>
+                                Professor
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='footer'>
@@ -43,6 +55,60 @@ function Dashboard() {
                 <div className='sideBar__item2'>
                     <img src='https://goglobiliti.com/assets/icons/Sign-out.svg' alt='logo'/>
                         Log out
+                </div>
+            </div>
+        </div>
+        <div className='dashboardBody'>
+            <div className='header'>
+                <div className='headerName'>
+                    Name
+                </div>
+                <div className='headerTabs'>
+                    <div className='headerTab'>
+                        DISCOVER
+                    </div>
+                    <div className='headerTab1'>
+                        IMPACT BOARD
+                    </div>
+                    <div className='headerTab'>
+                        LEARNING PODS
+                    </div>
+                </div>
+            </div>
+            <div className='body'>
+                <div className='bodyItems'>
+                    Impact Campaings
+                    <img src='https://goglobiliti.com/assets/icons/plus.svg' alt='add' />
+                    <div className='bodyItem'>
+                        <div className='itemTitle'>
+                            XYZ
+                        </div>
+                        <div className='badge'>
+                            Medium
+                        </div>
+                        <div className='itemDetails'>
+                            <div className='target'>
+                                <img src='https://goglobiliti.com/assets/icons/User.svg' alt='user' />
+                                Student
+                            </div>
+                            <div className='target'>
+                                <img src='https://goglobiliti.com/assets/icons/Link.svg' alt='user' />
+                                tools
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='bodyItems'>
+                    Learning Tools
+                    <img src='https://goglobiliti.com/assets/icons/plus.svg' alt='add' />
+                </div>
+                <div className='bodyItems'>
+                    Tool Pilots
+                    <img src='https://goglobiliti.com/assets/icons/plus.svg' alt='add' />
+                </div>
+                <div className='bodyItems'>
+                    Design Learning Pods
+                    <img src='https://goglobiliti.com/assets/icons/plus.svg' alt='add' />
                 </div>
             </div>
         </div>
